95ada35bfdab7227efb579d8e5853b1f2f9bc2b4
getkhuna <143786523+getkhuna@users.noreply.github.com>
2025-04-16 23:21:53 +0700

    Add PDF download section as a component

--- src/App.jsx
+++ src/App.jsx
@@ -71,17 +71,29 @@ export default function App() {
         </div>
       </section>
 
-      <section className="bg-[#e75d5d] py-12 px-4 flex flex-col h-[450px]">
-  <h2 className="font-serif text-white text-2xl mb-6">DOWNLOAD</h2>
-  <a
-    href="https://github.com/mindctd/Check-Spam-Email.git"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="border border-white text-white rounded-lg px-4 py-2 w-fit hover:bg-white hover:text-[#e75d5d] transition"
-  >
-    Download AI(version 1)
-  </a>
-</section>
+    <section className="bg-[#e75d5d] py-12 px-4 flex flex-col h-[450px]">
+      <h2 className="font-serif text-white text-2xl mb-6">DOWNLOAD</h2>
+
+      <a
+        href="https://github.com/mindctd/Check-Spam-Email.git"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="border border-white text-white rounded-lg px-4 py-2 w-fit hover:bg-white hover:text-[#e75d5d] transition mb-4"
+      >
+        Download AI (version 1)
+      </a>
+
+      <a
+        href="src\components\How to download Xspam.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="border border-white text-white rounded-lg px-4 py-2 w-fit hover:bg-white hover:text-[#e75d5d] transition"
+      >
+        How to dowload Xspam
+      </a>
+    </section>
+
+
 
       {/* --- Footer Section --- */}
       <footer className="bg-[#2f2626] text-white py-10 px-6">
